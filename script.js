4e399950ebbcd148b19d3f03e5e073386ef8e30f
JeffKolinMiranda <142186368+Jeffkolin7@users.noreply.github.com>
2024-07-04 18:47:43 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('prankButton').addEventListener('click', function() {
+    document.getElementById('prankMessage').style.display = 'block';
+    document.getElementById('prankButton').style.display = 'none';
+});
